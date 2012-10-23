aefe1bc9a5023b71a64395b94e94c4ef7983103d
Alonso Robles <alonso@alonsorobles.com>
2012-10-23 09:30:18 -0500

    Exercise 1

---
+++ HelloWorld/HelloWorld/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus;
+using log4net;
+
+namespace HelloWorld
+{
+    public class Class1: IConfigureThisEndpoint, IWantToRunAtStartup
+    {
+        public void Run()
+        {
+            LogManager.GetLogger("Class1").Info("Hello World!");
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
