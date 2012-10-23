392c7486ef1014f2fd352d7c34ec4020909b6f16
Alonso Robles <alonso@alonsorobles.com>
2012-10-23 15:02:44 -0500

    Exercise 9 - Authorization

--- HelloWorld/HelloWorld/MessageSender.cs
+++ HelloWorld/HelloWorld/MessageSender.cs
@@ -4,16 +4,17 @@ using log4net;
 
 namespace HelloWorld
 {
-    public class MessageSender :IWantToRunAtStartup
+    public class MessageSender : IWantToRunAtStartup
     {
+        public IBus Bus { get; set; }
+
         public void Run()
         {
+            Bus.OutgoingHeaders["user"] = "udi";
             var message = new Request {SaySomething = "Say something"};
             Bus.Send(message);
             LogManager.GetLogger("MessageSender").Info("Sent message");
         }
-
-        public IBus Bus { get; set; }
 
         public void Stop()
         {
--- HelloWorld/HelloWorldServer/EndpointConfig.cs
+++ HelloWorld/HelloWorldServer/EndpointConfig.cs
@@ -2,13 +2,18 @@
 
 namespace HelloWorldServer
 {
-    public class EndpointConfig: IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    public class EndpointConfig: IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization, ISpecifyMessageHandlerOrdering
     {
         public void Init()
         {
             Configure.With()
                 .DefaultBuilder()
                 .XmlSerializer("http://acme.com");
+        }
+
+        public void SpecifyOrder(Order order)
+        {
+            order.SpecifyFirst<Auth>();
         }
     }
 }
