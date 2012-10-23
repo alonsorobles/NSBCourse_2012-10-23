53f46d12f5c70777b3bfb2e59bf7f22d90621041
Alonso Robles <alonso@alonsorobles.com>
2012-10-23 16:53:00 -0500

    Exercise 12 - Overriding Configuration

---
+++ HelloWorld/SecurityServiceAdapter/RijndaelEncryptionServiceConfigOverride.cs
@@ -0,0 +1,17 @@
+﻿using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+namespace SecurityServiceAdapter
+{
+    public class RijndaelEncryptionServiceConfigOverride : IProvideConfiguration<RijndaelEncryptionServiceConfig>
+    {
+        public RijndaelEncryptionServiceConfig GetConfiguration()
+        {
+            return new RijndaelEncryptionServiceConfig
+                {
+                    // this key could be fetched from a REST/WS/DB call
+                    Key = "gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e7"
+                };
+        }
+    }
+}
