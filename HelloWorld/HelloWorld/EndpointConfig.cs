07c48d569e06b46b47478f192f0dfdc232654b40
Alonso Robles <alonso@alonsorobles.com>
2012-10-23 11:09:15 -0500

    Exercise 4 - Custom XML Namespace

--- HelloWorld/HelloWorld/EndpointConfig.cs
+++ HelloWorld/HelloWorld/EndpointConfig.cs
@@ -2,7 +2,13 @@
 
 namespace HelloWorld
 {
-    public class EndpointConfig: IConfigureThisEndpoint, AsA_Client
+    public class EndpointConfig: IConfigureThisEndpoint, AsA_Client, IWantCustomInitialization
     {
+        public void Init()
+        {
+            Configure.With()
+                .DefaultBuilder()
+                .XmlSerializer("http://acme.com");
+        }
     }
 }
